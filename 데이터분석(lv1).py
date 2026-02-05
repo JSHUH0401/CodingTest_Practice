647cb9493421c93e0f3a4ac6892cd62b096ce08c
JSHUH <john04010331@gmail.com>
2026-02-06 00:30:49 +0900

    데이터분석 문제 보완

--- 데이터분석(lv1).py
+++ 데이터분석(lv1).py
@@ -1,39 +1,9 @@
 def solution(data, ext, val_ext, sort_by):
+    by = [ "code", "date", "maximum", "remain" ]
     answer = []
-    ext_ind = 0
-    sort_ind = 0
-    if ext == "code":
-        ext_ind = 0
-    elif ext == "date":
-        ext_ind = 1
-    elif ext == "maximum":
-        ext_ind = 2
-    else:
-        ext_ind = 3
-    if sort_by == "code":
-        sort_ind = 0
-    elif sort_by == "date":
-        sort_ind = 1
-    elif sort_by == "maximum":
-        sort_ind = 2
-    else:
-        sort_ind = 3
-    real = []
-    lst = []
-    for cell in data:
-        if cell[ext_ind] < val_ext:
-            answer.append(cell)
-            lst.append(cell[sort_ind])
-    
-    for i in range(len(answer)):
-        for cell in answer:
-            if cell[sort_ind] == min(lst):
-                real.append(cell)
-                answer.pop(answer.index(cell))
-                lst.pop(lst.index(min(lst)))
-                print(lst)
-                if len(lst) == 0:
-                    break 
-    return real
-
+    for item in data:
+         if item[by.index(ext)] < val_ext:
+              answer.append(item)
+    return sorted(answer, key = lambda x: x[by.index(sort_by)])
+#lambda는 함수를 대신하는 것. KEY 뒤에 나오는 것은 그걸 기준으로 정렬한다는 뜻. 3차원 배열이어도 동일하다.
 print(solution([[1, 20300104, 100, 80], [2, 20300804, 847, 37], [3, 20300401, 10, 8]], "date",20300501,"remain"))
